a5250c2d69a805159c917d3742a51e8eae29baf8
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2023-12-18 21:01:53 -0300

    Arquivo alterado

--- php/suport.php
+++ php/suport.php
@@ -1,5 +1,5 @@
 <?php
-//Criação do cadastro para suporte . Este script cria para que a senha do suporte receba o tratamento de hash e seja inserido no banco criptografado
+//Criação do cadastro para suporte . Este script cria para que a senha do suporte receba o tratamento de hash e seja inserido no banco criptografado. Porém talvez dê para adicionar o usuário de suporte por aqui.
 $local = '127.0.0.1';
 $user = 'root';
 $pass = "";
