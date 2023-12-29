206f479922accdab48a080170f1f8d3958ef869b
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2023-12-29 20:17:10 -0300

    Update setor.js

--- javascript/setor.js
+++ javascript/setor.js
@@ -5,15 +5,5 @@ function Addition(){
 
     let godfather = document.querySelector("body")
     godfather.appendChild(input)
-
-    let inp_val = input.value
-
-    if(inp_val.length > 3){
-        let button_save = document.createElement("button")
-        button_save.type = 'submit'
-        button_save.className = 'save'
-
-        godfather.appendChild(button_save)
-        /*Não está aparecendo o botão, tentei por em uma div existente mas não funcionou*/
     }
 }
