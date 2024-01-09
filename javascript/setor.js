1a0b14bdbdffd2e6cd5c79d4d26d14d28012e23e
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-01-08 22:45:07 -0300

    Arquivos adicionados

--- javascript/setor.js
+++ javascript/setor.js
@@ -1,8 +1,14 @@
-function Addition(){
+
+
+    function Addition(){
 
     let input = document.createElement("input")
     input.className = 'setor'
 
     let godfather = document.querySelector("body")
     godfather.appendChild(input)
-}
+
+     /*Preciso que a condicional só deixe criar um novo input depois do primeiro ser salvo*/
+    
+    }
+
