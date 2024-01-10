8135749a9db0e056b733d3d2e9d5d16687499b7d
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-01-10 13:21:23 -0300

    Botões adicionados

--- javascript/setor.js
+++ javascript/setor.js
@@ -1,14 +1,36 @@
 
+function Addition(){
 
-    function Addition(){
+/*Criação dos elementos: input, botão de salvar e o botão para deletar*/
+let input = document.createElement("input")
+let button_save = document.createElement('button')
+let button_del = document.createElement('img')
+let godfather = document.querySelector("body")
+//Quebra de linha
+let line = document.createElement('br')
 
-    let input = document.createElement("input")
-    input.className = 'setor'
+input.className = 'setor'
+godfather.appendChild(input)
 
-    let godfather = document.querySelector("body")
-    godfather.appendChild(input)
+button_save.className = 'save'
+button_save.textContent = 'salvar'
+godfather.appendChild(button_save)
 
-     /*Preciso que a condicional só deixe criar um novo input depois do primeiro ser salvo*/
-    
-    }
+button_del.className = 'del'
+button_del.src = 'imgs-cad/cancel.svg'
+godfather.appendChild(button_del)
+
+godfather.appendChild(line)
+
+/*Se o input existir cria mais um*/
+if(input){
+    document.addEventListener("click", function(){
+    let count = 0
+    count++
+    let button = document.querySelector('#add')
+    button.style.transform = 'translateY(50vh)'
+    console.log(count)
+});
+}
+}
 
