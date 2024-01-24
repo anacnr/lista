195a50d5513d8571ff20b97978997befca021e8f
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-01-24 16:14:49 -0300

    Arquivo do setor

--- javascript/setor.js
+++ javascript/setor.js
@@ -1,37 +1,50 @@
 /*O chat me aconselhou a criar uma outra função para criar os elementos um de cada vez, somente quando o botão salvar for clicado que deixa criar mais input do setor*/
 
+    /*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha*/
+    let input = document.createElement("input")
+    let button_save = document.createElement('button')
+    let button_del = document.createElement('img')
+    let godfather = document.querySelector("body")//Elemento pai(padrinho)
+    let line = document.createElement('br')
+
+    input.className = 'setor'
+    godfather.appendChild(input)
+                
+    button_save.className = 'save'
+    button_save.textContent = 'salvar'
+    button_save.type = 'submit'
+    godfather.appendChild(button_save)
+                
+    button_del.className = 'del'
+    button_del.src = 'imgs-cad/cancel.svg'//ícone retirado do GoogIcons
+    godfather.appendChild(button_del)
+                
+    godfather.appendChild(line)
+    
+/*
 function Addition(){
 
-/*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha*/
-let input = document.createElement("input")
-let button_save = document.createElement('button')
-let button_del = document.createElement('img')
-let godfather = document.querySelector("body")//Elemento pai(padrinho)
-let line = document.createElement('br')
-
-input.className = 'setor'
-godfather.appendChild(input)
+    /*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha
+    let input = document.createElement("input")
+    let button_save = document.createElement('button')
+    let button_del = document.createElement('img')
+    let godfather = document.querySelector("body")//Elemento pai(padrinho)
+    let line = document.createElement('br')
             
-button_save.className = 'save'
-button_save.textContent = 'salvar'
-button_save.type = 'submit'
-godfather.appendChild(button_save)
-            
-button_del.className = 'del'
-button_del.src = 'imgs-cad/cancel.svg'//ícone retirado do GoogIcons
-godfather.appendChild(button_del)
-            
-godfather.appendChild(line)
-
-/*Função do botão*/
-button_save.addEventListener("click", function(){
-
-    console.log("Salvo!")//Teste
-
-});//Botão de salvar
-
-button_del.addEventListener("click" , function(){
-    console.log("Excluído")
-});//Botão deletar
-
+    input.className = 'setor'
+    godfather.appendChild(input)
+                
+    button_save.className = 'save'
+    button_save.textContent = 'salvar'
+    button_save.type = 'submit'
+    godfather.appendChild(button_save)
+                
+    button_del.className = 'del'
+    button_del.src = 'imgs-cad/cancel.svg'//ícone retirado do GoogIcons
+    godfather.appendChild(button_del)
+                
+    godfather.appendChild(line)
+                      
 }//Função principal
+
+*/
