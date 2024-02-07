290b0edd3796daed0b41774232051114eb2f4202
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-02-07 17:57:13 -0300

    Arquivos removidos

--- javascript/setor.js
+++ javascript/setor.js
@@ -1,65 +1,96 @@
 /*O chat me aconselhou a criar uma outra função para criar os elementos um de cada vez, somente quando o botão salvar for clicado que deixa criar mais input do setor. CRUD*/
 
-document.addEventListener("DOMContentLoaded", function(){
+console.log("Script carregado!");
 
-console.log("Script")
+/*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha*/
+let input = document.createElement("input");
+let button_save = document.createElement("button");
+let button_del = document.createElement("img");
+let godfather = document.querySelector("body"); //Elemento pai(padrinho)
+let line = document.createElement("br");
 
-    /*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha*/
-    let input = document.createElement("input")
-    let button_save = document.createElement('button')
-    let button_del = document.createElement('img')
-    let godfather = document.querySelector("form")//Elemento pai(padrinho)
-    let line = document.createElement('br')
+input.className = "setor";
+input.maxLength = "15";
+input.name = "setor"; //Utilizar para salvar no php
+godfather.appendChild(input);
 
-    input.className = 'setor'
-    input.maxLength = '15'
-    input.name = 'setor' //Utilizar para salvar no php
-    godfather.appendChild(input)
-                
-    button_save.className = 'save'
-    button_save.textContent = 'salvar'
-    button_save.type = 'submit'
-    godfather.appendChild(button_save)
-                
-    button_del.className = 'del'
-    button_del.src = 'imgs-cad/cancel.svg'//ícone retirado do GoogIcons
-    godfather.appendChild(button_del)
-                
-    godfather.appendChild(line)
+button_save.className = "save";
+button_save.textContent = "salvar";
+button_save.type = "submit";
+godfather.appendChild(button_save);
 
-document.querySelector('form').addEventListener("submit", function(e){
+button_del.className = "del";
+button_del.src = "imgs-cad/cancel.svg"; //ícone retirado do GoogIcons
+godfather.appendChild(button_del);
 
-            e.preventDefault();
+godfather.appendChild(line);
 
-            const form = new FormData()
-            form.append('setor' , input.value);
+//Cria a função para adicionar
 
-            //ajax
-            $.ajax({
-                url: '../php/setor.php',
-                type: 'POST',
-                data: form,
-                processData: false, contentType: false
-            }).done(function(result){
-                /*O result vai valer como o $request*/
-                if(result == "true"){
-                    console.log("Teste 1: " , result)
-                    //Vai ter a função de adicionar e de excluir.
-                }
-                else{
-                    console.log("Teste 2: " , result)
-                    //Cria uma linha para avisar que o setor não foi cadastradado por causa do requisito.
-                    let phrase = document.createElement("p")
-                    phrase.id = 'phrase'
-                    phrase.innerHTML = 'Necessário nome com mais de 3 letras'
-                    godfather.appendChild(phrase)
-                    setTimeout(()=>{
-                        phrase.style.display = 'none'
-                    },4000)
-                }
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                /*O .fail() serve para mostrar o erro caso exista*/
-                console.log("Erro na requisição AJAX:", textStatus, errorThrown);
-            });//ajax //done //fail          
-        });//submit
-});//DOMContentLoaded
+const addition = function toAdd() {
+  /*Criação dos elementos: input, botão de salvar,o botão para deletar e a quebra de linha*/
+  let input = document.createElement("input");
+  let button_save = document.createElement("button");
+  let button_del = document.createElement("img");
+  let godfather = document.querySelector("body"); //Elemento pai(padrinho)
+  let line = document.createElement("br");
+
+  input.className = "setor";
+  input.maxLength = "15";
+  input.name = "setor"; //Utilizar para salvar no php
+  godfather.appendChild(input);
+
+  button_save.className = "save";
+  button_save.textContent = "salvar";
+  button_save.type = "submit";
+  godfather.appendChild(button_save);
+
+  button_del.className = "del";
+  button_del.src = "imgs-cad/cancel.svg"; //ícone retirado do GoogIcons
+  godfather.appendChild(button_del);
+
+  godfather.appendChild(line);
+
+};//Função addition
+
+/*LocalStorage dos setores para criar o CRUD*/
+
+//Nomeação do setor(variável global)
+const nameSection = {
+  //let name_setor = input.value
+  name: "setora",
+  id: "3",
+};
+/*Métodos do localStorage*/
+const getItem = () => JSON.parse(localStorage.getItem("name")) ?? [];
+
+const setItem = (add_name) =>
+  localStorage.setItem("name", JSON.stringify(add_name));
+
+//Criação do setor em JSON
+function newSection(sector) {
+  let add_name = getItem();
+  add_name.push(sector);
+  setItem(add_name);
+}
+
+//Leitura do setor em JSON
+
+const readSection = () => getItem();
+
+//function readSection(){ getItem(); } Não pegou
+
+//Atualização do setor em JSON
+
+function upSetor(setor, index) {
+  const up_sector = readSection();
+  up_sector[index] = setor;
+  setItem(up_sector);
+}
+
+//Exclusão do setor em JSON
+function delSetor(index){
+    const del_sector = readSection()
+    del_sector.splice(index,1)
+    setItem(del_sector)
+}
