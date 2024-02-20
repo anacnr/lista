d7f283614fc221ca5782c58d6007b2af1ca8e02d
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-02-20 12:22:20 -0300

    Arquivos adicionados

--- php/exibi-set.php
+++ php/exibi-set.php
@@ -14,7 +14,9 @@ $query = mysqli_query($mysql, $select);
 if (mysqli_num_rows($query) > 0) {
 
     $lines = mysqli_num_rows($query);//Número de linhas contidas na tabela setor
-
+    while ($row = mysqli_fetch_assoc($query)) { 
+       echo "<span class = 'name'>" . $row['nome'] . "</span>";
+    }
 }       
 else {
     echo "Nenhum resultado encontrado.";
@@ -38,24 +40,22 @@ mysqli_close($mysql);
     <link rel="stylesheet" href="../css/set-vendedor/mobile.css">
 </head>
 <body>
-    
-    <span class="name"> <?php while ($row = mysqli_fetch_assoc($query)) { echo $row['nome'];} ?> </span>
+    <img src="../supermercado/buttons/edit_square_FILL0_wght400_GRAD0_opsz24.svg" alt="botao-edita" width="50px">
+
+    <img src="../supermercado/buttons/delete_FILL0_wght400_GRAD0_opsz24.svg" alt="botao-apaga" width="50px
+    ">
 
     <script src="../javascript/jquery-3.7.1.min.js"></script>   
 <script>
-
     document.addEventListener("DOMContentLoaded", function(){
         console.log('Script carregado!')
-
     let nome = document.querySelector('.name')
-
     let setor = new FormData();
     setor.append('nome', nome)
-
     $.ajax({
         url: 'exibi-set.php' , type: 'POST' ,data:setor , processData: false ,contentType: false  
     }).done(function(request){
-       // console.log(request)
+       
     }).fail(function(jqXHR, textStatus, errorThrown){
         console.log(" ERRO " , jqXHR, textStatus, errorThrown)
     })//ajax
