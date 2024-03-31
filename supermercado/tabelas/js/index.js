7e7ccab13ecb22b111bd2909ebaebd1d3a5650d3
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-03-31 19:25:27 -0300

    Arquivo js

--- supermercado/tabelas/js/index.js
+++ supermercado/tabelas/js/index.js
@@ -13,7 +13,7 @@ const add = function toAdd(){
     let table_body = document.querySelector('tbody')
     table_body.appendChild(table_row)
     //Este laço serve para criar tds com a quantidade de ths
-    for(let count = 0; count < 6; count++){
+    for(let count = 0; count < 5; count++){
             let table_data = document.createElement('td')
             table_row.appendChild(table_data)
 
@@ -26,30 +26,95 @@ const add = function toAdd(){
     }
 
     //Input file para receber a imagem
-    let table_data = document.createElement('td')
-    table_row.appendChild(table_data)
+    let table_data_number = document.createElement('td')
+    table_row.appendChild(table_data_number)
+
+    let input_numbers = document.createElement("input")
+    input_numbers.type = 'number'
+    input_numbers.className = 'input'
+    input_numbers.required = true
+
+    table_data_number.appendChild(input_numbers)
+
+    //Input file para receber a imagem
+    let table_data_img = document.createElement('td')
+    table_row.appendChild(table_data_img)
 
     let input = document.createElement("input")
         input.type = 'file'
         input.id = 'input-img'
         input.className = 'input'
+        input.accept = 'image/*'
         input.required = true
 
-        table_data.appendChild(input)
+        table_data_img.appendChild(input)
 
      //Label do input file
      let label = document.createElement("label")
      label.className = 'label-input'
      label.htmlFor = `input-img`
 
-     table_data.appendChild(label)
+     table_data_img.appendChild(label)
 
      //Ìcone de upload
      let icon = document.createElement("img")
+     icon.id = 'icon-upload'
      icon.className = 'icon-upload'
      icon.src = 'buttons/upload.svg'
     
      label.appendChild(icon)
+
+    //Parte para mostrar a imagem inserida
+    let img_uploaded = document.getElementById("input-img")
+
+    img_uploaded.addEventListener("change" , function(){
+    let label_icon = document.getElementById("icon-upload")
+    label_icon.classList.remove('icon-upload')
+
+    let eyes = new FileReader()
+
+    eyes.onload = function toRead(){
+
+        label_icon.src = `${eyes.result}`
+        label_icon.classList.add('img-uploaded')
+    }
+    eyes.readAsDataURL(img_uploaded.files[0]);
+});//Função da imagem
+
+    //Parte de salvar os setores
+
+    let button_save = document.querySelector("#sav")
+
+    const save = function toSave(){
+    
+        //Verifica se os inputs não receberam valores
+        let datas = document.querySelectorAll('.input')[0,1,2,3,4,5,6].value
+
+        if(datas.length == 0){
+            console.log("Por favor preencha os campos!")
+        }
+        else{
+            //É aqui que vai rolar a adição dos produtos
+            console.log("Salvo")
+
+            const form = new FormData();
+            form.append('product' , datas[0])
+            form.append('code' , datas[1])
+            form.append('measure' , datas[2])
+            form.append('price' , datas[3])
+            form.append ('brand' , datas[4])
+            form.append('quantity' , datas[5])
+            form.append('image' , datas[6])
+
+            $.ajax({
+                url : '' , type: 'POST' ,data: form, processData: false ,contentType: false
+            })
+        }
+        
+        //console.log("Salvo")
+    }
+
+    button_save.addEventListener("click" , save)
 
 }//Função para adicionar.
 
