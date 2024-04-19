350926081dc7cec1340c76924efce5f4320e178c
Ana Carolina <136374582+anacnr@users.noreply.github.com>
2024-04-19 16:11:29 -0300

    Arquivo php

--- php/promo.php
+++ php/promo.php
@@ -5,23 +5,34 @@ if($mysql->connect_error != null){
     die("Erro na conexão" . $mysql->error);
 }
 else{
-  
-    $select = "SELECT * FROM produto WHERE id = 1"; // Seleciona apenas 'nome'
-    $query = mysqli_query($mysql, $select);
-    
-    $request = [];
-    
-    if ($query) {
-        while ($row = mysqli_fetch_assoc($query)) {
-            $request[] = array("nome" =>$row['nome']); // Adiciona o resultado ao array $request
+
+    $input = $_POST['product'];
+
+    if(!empty($input)){
+        //Se o valor do input não estiver vazio vai realizar a pesquisa
+        $select = "SELECT * FROM produto WHERE nome LIKE '%$input%'";
+
+        $query = mysqli_query($mysql, $select);
+
+        $request = [];
+
+        if (mysqli_num_rows($query) > 0) {
+
+            $rows = mysqli_num_rows($query);
+
+            while ($row = mysqli_fetch_assoc($query)) {
+                $request[] = array('nome' => $row['nome'], 'codigo' => $row['codigo'], 'marca' => $row['marca'], 'linhas' => $rows);
+            }
+            header('Content-Type: application/json');
+            echo json_encode($request);
+            exit;
+        } else {
+            echo "Nenhum resultado encontrado.";
         }
-        header('Content-Type: application/json');
-        echo json_encode($request);
-    } else {
-        echo "Erro na consulta: " . mysqli_error($mysql);
     }
-    
-    
+    else{
+        echo "Input vazio!";
+    }
     mysqli_close($mysql);   
 }
 ?>
