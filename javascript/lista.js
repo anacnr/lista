da332ee87a02c5c91071c81ed63365dc96f683c0
anacnr <136374582+anacnr@users.noreply.github.com>
2024-05-02 21:44:48 -0300

    Arquivos

--- javascript/lista.js
+++ javascript/lista.js
@@ -1,24 +1,13 @@
 /*Criar a lista com js*/
-//Segundo arquivo
-  //Fazer uma requisição Ajax para obter o conteúdo do arquivo
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const response = xhr.responseText;
-                //Aqui você pode manipular a resposta da requisição
-                document.addEventListener("DOMContentLoaded" , () =>{
-                    const tag = document.createElement("script")
-                    tag.src = 'search.js'
-                    document.addEventListener('choose' , function(event){
-                        const name_prod = event.detail;
-                        console.log("Conteúdo do arquivo:", name_prod);
-                    })
-                });               
-            } else {
-                console.error('Erro ao obter conteúdo do arquivo:', xhr.statusText);
-            }
-        }
-    };
-    xhr.open('GET', '../javascript/search.js');
-    xhr.send();
+document.addEventListener("DOMContentLoaded" , ()=>{
+    
+        document.addEventListener('choose', (event) => {
+
+            const produtoEscolhido = event.detail;
+            console.log(produtoEscolhido)
+            
+            // Agora você pode fazer qualquer coisa com o produto escolhido,
+            // como atualizar a lista ou realizar outra ação baseada nesse produto.
+        });
+    });
+       
