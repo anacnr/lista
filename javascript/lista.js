0578ac184c99764ca19f27a8c4191820beedbc00
anacnr <136374582+anacnr@users.noreply.github.com>
2024-05-04 14:17:14 -0300

    Tela da lista

--- javascript/lista.js
+++ javascript/lista.js
@@ -3,14 +3,45 @@ document.addEventListener("DOMContentLoaded" , ()=>{
     fetch("../javascript/lista.json").then(function(response){
         response.json().then(function(produto){
             /*Aqui deve fazer a lógica de criação da lista*/
-            //console.log(produto)
+            let quanti_row = Array(produto).length
+            let tablerow = document.createElement("tr")
+            for(let count=0;count<=quanti_row;count++){
+                document.querySelector('tbody').appendChild(tablerow)
+            }
+            for(let count=0;count<6;count++){
+                let tabledata = document.createElement("td")
+                tabledata.id = `td${count}`
+                tablerow.appendChild(tabledata)
+            }
             produto.produtos.map((ponto)=>{
                 console.log(ponto)
-                let li = document.createElement("li")
-                li.innerHTML += `${ponto.nome}`
-                document.querySelector('ul').appendChild(li)
+                document.querySelectorAll('td').forEach((element =>{
+                    if(element.id == 'td0'){
+                        element.textContent = `${ponto.nome}`
+                    }
+                    else if(element.id == 'td1'){
+                        element.textContent = `${ponto.peso}`
+                    }
+                    else if(element.id == 'td2'){
+                        element.textContent = parseFloat(`${ponto.valor}`).toFixed(2).replace('.', ',')
+                    }
+                    else if(element.id == 'td3'){
+                        element.textContent = `${ponto.marca}`
+                    }
+                    else if(element.id == 'td4'){
+                        element.textContent = 0
+                    }
+                    else{
+                        let img = document.createElement("img")
+                        img.src = '/imagens/produtos/frutas/berries-min.jpg'
+                        img.className = 'image'
+                        element.appendChild(img)
+                    }
+                }))
+                document.getElementById("total-span").innerHTML += parseFloat(`${ponto.valor}`).toFixed(2).replace('.', ',')
             })
         })
     }) 
 });
-       
+let ww = document.querySelector('tfoot>tr>td')
+console.log(ww);
