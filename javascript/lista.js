4f26bb0a691a4229db626fc72e3fc75312f3189d
anacnr <136374582+anacnr@users.noreply.github.com>
2024-05-10 22:13:17 -0300

    arquivos

--- javascript/lista.js
+++ javascript/lista.js
@@ -2,33 +2,43 @@
 document.addEventListener("DOMContentLoaded" , ()=>{
 fetch("../javascript/lista.json").then(function(response){
     response.json().then(function(produto){
-        let create_rows = produto.length
-        for(let count=1;count<=create_rows;count++){
-            let tablerow = document.createElement("tr");
-            tablerow.id = `tr${count}`
-            document.querySelector('tbody').appendChild(tablerow);
-        }
-        document.querySelectorAll('tbody > tr').forEach((iten,index)=>{
-            for(let count=0;count<5;count++){
-                let td = document.createElement("td")
-                td.id = `td${count}`
-                iten.appendChild(td)
-            }
-        })
 
-        document.querySelectorAll('tbody > tr').forEach((iten,index)=>{
-            let first = document.querySelectorAll('td')[0]
-            produto.map(ponto =>{               
-                let tr_id = iten.id
+// Obtém o tbody onde queremos adicionar as linhas
+let tbody = document.querySelector('tbody');
+
+// Itera sobre cada ponto do produto e adiciona uma nova linha para cada um
+produto.forEach((ponto) => {
+    // Cria uma nova linha
+    let create_row = document.createElement('tr');
     
-                if(tr_id == `tr${index}`){
-                    first.textContent = `${ponto.nome}`
-                    console.log(`${ponto.nome}`);
-                }
-              })
-        })
+    for(let count=0;count<5;count++){
+        let create_td = document.createElement("td")
+        create_td.id = `${count}`
+        create_row.appendChild(create_td)
 
-        
-       1})
+        if(create_td.id == 0){
+            create_td.innerHTML = `${ponto.nome}`
+        }
+        else if(create_td.id == 1){
+            create_td.innerHTML = `${ponto.peso}`
+        }
+        else if(create_td.id == 2){
+            create_td.innerHTML = `${ponto.valor}`
+        }
+        else if(create_td.id == 3){
+            create_td.innerHTML = `${ponto.marca}`
+        }
+        else{
+            let create_img = document.createElement("img")
+            create_img.src = `../php/${ponto.imagem}`
+            create_td.appendChild(create_img)
+        }
+    }
+    
+    // Adiciona a nova linha ao tbody
+    tbody.appendChild(create_row);
+});   
+    //console.log("TD: " + iten.id + "INDEX: " + index);
+      })
     })
-});//tehn
+});//then
