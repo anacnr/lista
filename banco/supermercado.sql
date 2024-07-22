28bfb3dc6ee2a475e64e36e97901f485ac433700
anacnr <136374582+anacnr@users.noreply.github.com>
2024-07-21 21:52:44 -0300

    Atualização do banco

--- banco/supermercado.sql
+++ banco/supermercado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/05/2024 às 16:26
+-- Tempo de geração: 22/07/2024 às 02:50
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,15 +35,17 @@ CREATE TABLE `comprador` (
   `telefone` varchar(14) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `senha_hash` varchar(255) DEFAULT NULL
+  `senha_hash` varchar(255) DEFAULT NULL,
+  `id_prod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `comprador`
 --
 
-INSERT INTO `comprador` (`nome`, `cpf`, `email`, `moradia`, `telefone`, `senha`, `id`, `senha_hash`) VALUES
-('ertinho', '22000000000', 'erto@email.com', 'Rua 1 avenida', '11100000000', '90g&!151', 21, '$2y$10$sKVHHNLHozrH2cfww9Pb.OtsUwnCE44H9I3C7ejzps4YQH0Cvzxw6');
+INSERT INTO `comprador` (`nome`, `cpf`, `email`, `moradia`, `telefone`, `senha`, `id`, `senha_hash`, `id_prod`) VALUES
+('ertinho', '22000000000', 'erto@email.com', 'Rua 1 avenida', '11100000000', '90g&!151', 1, '$2y$10$sKVHHNLHozrH2cfww9Pb.OtsUwnCE44H9I3C7ejzps4YQH0Cvzxw6', 1),
+('Carla', '20000000000000', 'user4@email.com', 'Rua agua', '(00) 9000-1', 'cliente12345', 2, '$2y$10$Si7VqML7/aFT5.s768Q6eubEdFe/NBl.5CWWipGvP2rj.8Wrv85Ma', 4);
 
 -- --------------------------------------------------------
 
@@ -60,21 +62,19 @@ CREATE TABLE `produto` (
   `marca` varchar(255) DEFAULT NULL,
   `quantidade` varchar(50) DEFAULT NULL,
   `imagem` varchar(255) DEFAULT NULL,
-  `setor` varchar(50) NOT NULL
+  `setor` varchar(50) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_supermercado` int(11) NOT NULL,
+  `id_setor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `produto`
 --
 
-INSERT INTO `produto` (`id`, `nome`, `codigo`, `peso`, `valor`, `marca`, `quantidade`, `imagem`, `setor`) VALUES
-(1, 'maçã', 'código1', '300000', '5,80', 'teste1', '120000', 'produtos/661444420dd1b.jpg', ''),
-(2, 'maçã', 'código2', '40000', '3,90', 'teste2', '67000', 'produtos/661444e901cab.jpg', ''),
-(4, 'Laranja', 'Código3', '20000', '6,60', 'Teste3', '90000', 'produtos/663c000fe1980.jpg', ''),
-(5, 'limão', 'Código4', '30000', '5,55', 'Teste4', '20000', 'produtos/663c00b26e615.jpg', ''),
-(6, 'Carne', 'Código5', '40000', '37,89', 'Teste5', '50000', 'produtos/663c00f19db92.png', ''),
-(7, 'Picanha', 'Código6', '80000', '45,67', 'Teste6', '120000', 'produtos/663c01542538a.png', ''),
-(8, 'Couve-flor', 'Código7', '10000', '13,80', 'Teste7', '30000', 'produtos/663c01a982782.jpg', '');
+INSERT INTO `produto` (`id`, `nome`, `codigo`, `peso`, `valor`, `marca`, `quantidade`, `imagem`, `setor`, `id_cliente`, `id_supermercado`, `id_setor`) VALUES
+(1, 'Detergente', 'código1', '500', '5,80', 'teste1', '120000', 'produtos/661444420dd1b.jpg', 'frutas', 1, 0, 2),
+(4, 'Laranja', 'Código3', '500', '6,60', 'Teste3', '90000', 'produtos/663c000fe1980.jpg', 'frutas', 2, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -92,7 +92,11 @@ CREATE TABLE `setor` (
 --
 
 INSERT INTO `setor` (`id`, `nome`) VALUES
-(1, 'limpeza');
+(1, 'variados'),
+(2, 'Limpeza'),
+(3, 'Hortifruti'),
+(4, 'Laticinio'),
+(6, 'Bebida');
 
 -- --------------------------------------------------------
 
@@ -121,25 +125,27 @@ INSERT INTO `suporte` (`id`, `email`, `senha`, `senha_hash`) VALUES
 --
 
 CREATE TABLE `vendedor` (
-  `empresa` varchar(100) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `empresa` varchar(100) NOT NULL,
   `responsavel` varchar(100) NOT NULL,
   `cnpj` char(18) NOT NULL,
-  `localizacao` varchar(60) DEFAULT NULL,
+  `localizacao` varchar(255) NOT NULL,
   `telfixo` varchar(14) DEFAULT NULL,
   `celular` varchar(14) DEFAULT NULL,
   `email` varchar(90) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
   `imagem` varchar(60) DEFAULT NULL,
-  `id` int(11) NOT NULL,
-  `senha_hash` varchar(255) DEFAULT NULL
+  `senha_hash` varchar(255) DEFAULT NULL,
+  `cadastro_data` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `vendedor`
 --
 
-INSERT INTO `vendedor` (`empresa`, `responsavel`, `cnpj`, `localizacao`, `telfixo`, `celular`, `email`, `senha`, `imagem`, `id`, `senha_hash`) VALUES
-('Empresa1', 'Zero', '1100000000000', 'Rua 1 Avenida', '0000000000', '10000000000', 'userteste20@email.com', 'abc12345', 'enviadas/65f1eaf5c0c10.jpg', 14, '$2y$10$x1WA2go9nrudOyMxdiyZUeC42EqFyuFTa5SiakHdbCPB5U27VEWEC');
+INSERT INTO `vendedor` (`id`, `empresa`, `responsavel`, `cnpj`, `localizacao`, `telfixo`, `celular`, `email`, `senha`, `imagem`, `senha_hash`, `cadastro_data`) VALUES
+(14, '', 'Zero', '1100000000000', 'Rua 1 Avenida', '0000000000', '10000000000', 'userteste20@email.com', 'abc12345', 'enviadas/65f1eaf5c0c10.jpg', '$2y$10$x1WA2go9nrudOyMxdiyZUeC42EqFyuFTa5SiakHdbCPB5U27VEWEC', '0000-00-00'),
+(15, '', 'Dedeu', '090000000000000000', 'rua 1 avenida', '1200000000', '12000000000', 'user6@email.com', 'superm12345', 'enviadas/663d619aedfee.png', '$2y$10$SRHGj3tgLRvqBbmUqCOV.OXS/K5/AUoHnNmGD6TzRmjQicklEMaSS', '0000-00-00');
 
 --
 -- Índices para tabelas despejadas
@@ -156,7 +162,9 @@ ALTER TABLE `comprador`
 -- Índices de tabela `produto`
 --
 ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_prod_setor` (`id_setor`),
+  ADD KEY `fk_cliente_prod` (`id_cliente`);
 
 --
 -- Índices de tabela `setor`
@@ -175,8 +183,7 @@ ALTER TABLE `suporte`
 -- Índices de tabela `vendedor`
 --
 ALTER TABLE `vendedor`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `gmail` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -186,19 +193,19 @@ ALTER TABLE `vendedor`
 -- AUTO_INCREMENT de tabela `comprador`
 --
 ALTER TABLE `comprador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `setor`
 --
 ALTER TABLE `setor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `suporte`
@@ -210,7 +217,18 @@ ALTER TABLE `suporte`
 -- AUTO_INCREMENT de tabela `vendedor`
 --
 ALTER TABLE `vendedor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `fk_cliente_prod` FOREIGN KEY (`id_cliente`) REFERENCES `comprador` (`id`),
+  ADD CONSTRAINT `fk_prod_setor` FOREIGN KEY (`id_setor`) REFERENCES `setor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
